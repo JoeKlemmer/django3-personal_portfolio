15fb90d38970c6e30284f0d1045b957612f946d4
Joe Klemmer <joe.klemmer@gmail.com>
2020-09-16 14:16:07 -0400

    Just added basic links between portfolio and blog

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Blog
 
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    # Reverse order listing first 5 entries
+    blogs = Blog.objects.order_by('-date')[:5]
     return render(request, "blog/all_blogs.html", {"blogs":blogs})
