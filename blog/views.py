8843e86a6a5b918e8e7b1bd73cbdea6668109641
Joe Klemmer <joe.klemmer@gmail.com>
2020-09-18 12:36:40 -0400

    Minor formatting of the site and some small adjuctments to the admin section for readability

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blogs(request):
+    # blog_count = Blog.objects.count
     # Reverse order listing first 5 entries
-    blogs = Blog.objects.order_by('-date')[:5]
+    # blogs = Blog.objects.order_by('-date')[:5]
+    blogs = Blog.objects.order_by('-date')
     return render(request, "blog/all_blogs.html", {"blogs":blogs})
 
 def detail(request, blog_id):
